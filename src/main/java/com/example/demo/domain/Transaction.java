f84a2bbb673b5122e4ff09c16badefe9ca70d27c
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:40 +0200

    feat: add EqualsAndHashCode annotation to Transaction.java

--- src/main/java/com/example/demo/domain/Transaction.java
+++ src/main/java/com/example/demo/domain/Transaction.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 @Entity
 @Table(name = "transactions")
 public class Transaction {
