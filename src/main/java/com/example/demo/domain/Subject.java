1bd1f2f5f66bd6a3b48e8e1c6afc92dbedb877f2
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:40 +0200

    fix: add proper sequence generator for Subject

--- src/main/java/com/example/demo/domain/Subject.java
+++ src/main/java/com/example/demo/domain/Subject.java
@@ -2,7 +2,6 @@ package com.example.demo.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -12,8 +11,6 @@ import lombok.Setter;
 
 import java.util.List;
 
-import static jakarta.persistence.GenerationType.SEQUENCE;
-
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -22,7 +19,6 @@ import static jakarta.persistence.GenerationType.SEQUENCE;
 public class Subject {
 
     @Id
-    @GeneratedValue(strategy = SEQUENCE, generator = "account_sequence")
     private Long id;
 
     /**
