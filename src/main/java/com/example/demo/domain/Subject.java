28e34de3741ad2bdecdc19cc8289a1212b391429
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:40 +0200

    feat: add explicit equals and hashCode methods to Subject entity as suggested by Intellij Idea due to performance

--- src/main/java/com/example/demo/domain/Subject.java
+++ src/main/java/com/example/demo/domain/Subject.java
@@ -1,25 +1,27 @@
 package com.example.demo.domain;
 
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
-@EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "subjects")
 public class Subject {
 
-  @Id
-  @GeneratedValue
-  private Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
     /**
      * Column names are defined in accordance with migration.sql.
@@ -32,4 +34,25 @@ public class Subject {
     @OneToMany(mappedBy = "subject")
     private List<BankAccount> accounts;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
+        Subject subject = (Subject) o;
+        return getId() != null && Objects.equals(getId(), subject.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
