cc7b46fa3bf70eb613bec5ac85dfb43b73ec6dd6
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:40 +0200

    fix: fix findById response in case of error is now correct NOT_FOUND instead of wrong BAD_REQUEST

--- src/main/java/com/example/demo/resource/SubjectResource.java
+++ src/main/java/com/example/demo/resource/SubjectResource.java
@@ -3,7 +3,6 @@ package com.example.demo.resource;
 import com.example.demo.api.CreateSubjectRequest;
 import com.example.demo.api.SubjectResponse;
 import com.example.demo.service.SubjectService;
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,25 +11,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RequiredArgsConstructor
 @RestController
 public class SubjectResource {
 
-  private final SubjectService subjectService;
+    private final SubjectService subjectService;
 
-  @PostMapping("/subjects")
-  public ResponseEntity<Void> create(@RequestBody CreateSubjectRequest request) {
-    return ResponseEntity.created(URI.create("http://localhost:8080/subjects/" + subjectService.save(request))).build();
-  }
-
-  @GetMapping("/{id}")
-  public ResponseEntity<SubjectResponse> findById(@PathVariable Long id) {
-    final var result = subjectService.findById(id);
-
-    if (result.isEmpty()) {
-      return ResponseEntity.badRequest().build();
-    } else {
-      return ResponseEntity.ok(result.get());
+    @PostMapping("/subjects")
+    public ResponseEntity<Void> create(@RequestBody CreateSubjectRequest request) {
+        return ResponseEntity.created(URI.create("http://localhost:8080/subjects/" + subjectService.save(request))).build();
     }
-  }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SubjectResponse> findById(@PathVariable Long id) {
+        var result = subjectService.findById(id);
+
+        return result.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
