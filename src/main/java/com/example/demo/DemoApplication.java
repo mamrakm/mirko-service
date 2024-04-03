7cfcb2d22b67206bfbc7c8c49c81461b3e5e42ef
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:40 +0200

    feat: SLF4J annotation to DemoApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
   public static void main(String[] args) {
