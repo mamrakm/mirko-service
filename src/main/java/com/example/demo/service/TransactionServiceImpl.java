27032f87c289653ec2a6ec4d9c72a5406d3c432e
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:39 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/service/TransactionServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.api.PostTransactionRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+    @Override
+    public void createTransaction(Long account, PostTransactionRequest request) {
+
+    }
+}
