1ff9aa7cd6539e7e82897c9b41719094a59abcc3
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:40 +0200

    feat: change 'name' field to 'firstName' for better readability

--- src/main/java/com/example/demo/api/SubjectResponse.java
+++ src/main/java/com/example/demo/api/SubjectResponse.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class SubjectResponse {
 
   private Long id;
-  private String name;
+  private String firstName;
   private String lastName;
 
   private int numberOfAccounts;
