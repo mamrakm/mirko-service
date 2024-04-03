26f56be6551cc211c3ac20574c783fde79fcc9d6
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:40 +0200

    fix(API): fixes accidentally changed response

--- src/main/java/com/example/demo/api/SubjectResponse.java
+++ src/main/java/com/example/demo/api/SubjectResponse.java
@@ -1,13 +1,16 @@
 package com.example.demo.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class SubjectResponse {
 
-  private Long id;
-  private String firstName;
-  private String lastName;
+    private Long id;
+    @JsonProperty("name")
+    private String firstName;
 
-  private int numberOfAccounts;
+    private String lastName;
+
+    private int numberOfAccounts;
 }
