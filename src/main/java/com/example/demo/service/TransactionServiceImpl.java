b885305bced507a5f9d167acfa9da6c19f953b3d
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:40 +0200

    feat: implement createTransaction in TransactionServiceImpl as specified by Javadoc

--- src/main/java/com/example/demo/service/TransactionServiceImpl.java
+++ src/main/java/com/example/demo/service/TransactionServiceImpl.java
@@ -1,12 +1,55 @@
 package com.example.demo.service;
 
 import com.example.demo.api.PostTransactionRequest;
+import com.example.demo.domain.Transaction;
+import com.example.demo.exception.InternalTransactionException;
+import com.example.demo.repository.BankAccountRepository;
+import com.example.demo.repository.TransactionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
+@Slf4j
 @Service
 public class TransactionServiceImpl implements TransactionService {
-    @Override
-    public void createTransaction(Long account, PostTransactionRequest request) {
+    private BankAccountRepository bankAccountRepository;
+    private TransactionRepository transactionRepository;
 
+    @Override
+    public void createTransaction(Long id, PostTransactionRequest request) {
+        if (null == id) {
+            log.warn("id must not be null", new InternalTransactionException());
+            return;
+        }
+
+        var account = bankAccountRepository.findById(id).orElse(null);
+        if (null == account) {
+            log.warn("No subject with [id={}] found", id, new InternalTransactionException());
+            return;
+        }
+
+        var accountBalanceBeforeTxn = account.getBalance();
+        if (null == accountBalanceBeforeTxn) {
+            log.warn("There is no record matching id [id={}]", id, new InternalTransactionException());
+            return;
+        }
+
+        var reminder = accountBalanceBeforeTxn.add(request.getAmount());
+        if (reminder.compareTo(BigDecimal.ZERO) < 0) {
+            throw new InternalTransactionException();
+        }
+        var txn = new Transaction();
+
+        if (reminder.compareTo(BigDecimal.valueOf(200)) < 0) {
+            txn.setAmount(reminder);
+            txn.setBankAccount(account);
+            transactionRepository.saveAndFlush(txn);
+            throw new InternalTransactionException();
+        }
+
+        txn.setAmount(reminder);
+        txn.setBankAccount(account);
+        transactionRepository.saveAndFlush(txn);
     }
 }
