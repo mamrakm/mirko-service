e165fce854777af3058e42ba607854829aae733e
EMeMSoft s.r.o <info@ememsoft.cz>
2024-04-03 16:19:40 +0200

    feat: get back to Lombok generated equals and hashcode for readability; intendation fix

--- src/main/java/com/example/demo/domain/BankAccount.java
+++ src/main/java/com/example/demo/domain/BankAccount.java
@@ -1,58 +1,37 @@
 package com.example.demo.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.proxy.HibernateProxy;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 @Entity
 @Table(name = "bank_accounts")
 public class BankAccount {
 
-  @Id
-  @GeneratedValue
-  private Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
-  private String prefix;
-  private String suffix;
+    private String prefix;
+    private String suffix;
 
-  private boolean applyForLoan;
+    private boolean applyForLoan;
 
-  private BigDecimal balance;
+    private BigDecimal balance;
 
-  @ManyToOne
-  @JoinColumn(name = "subject")
-  private Subject subject;
-
-  @Override
-  public final boolean equals(Object o) {
-    if (this == o) {
-        return true;
-    }
-    if (o == null) {
-        return false;
-    }
-    Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-    Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-    if (thisEffectiveClass != oEffectiveClass) {
-        return false;
-    }
-    BankAccount that = (BankAccount) o;
-    return getId() != null && Objects.equals(getId(), that.getId());
-  }
-
-  @Override
-  public final int hashCode() {
-    return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-  }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject")
+    private Subject subject;
 }
--- src/main/java/com/example/demo/domain/Subject.java
+++ src/main/java/com/example/demo/domain/Subject.java
@@ -6,15 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.proxy.HibernateProxy;
 
 import java.util.List;
-import java.util.Objects;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 @Entity
 @Table(name = "subjects")
 public class Subject {
@@ -33,26 +33,4 @@ public class Subject {
 
     @OneToMany(mappedBy = "subject")
     private List<BankAccount> accounts;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null) {
-            return false;
-        }
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) {
-            return false;
-        }
-        Subject subject = (Subject) o;
-        return getId() != null && Objects.equals(getId(), subject.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
